71bd6045b6207a92a6e9026d9cd3b2d632875dec
germannp <philipp.germann@embl.es>
2024-12-30 12:50:41 +0100

    Exclude intros, covers, and live versions
    Turned out these kind of songs are incredibly hard to date. To test just
    use Blind Guardian 🙄

--- last-hitster.py
+++ last-hitster.py
@@ -27,6 +27,8 @@ LASTFM_API_SECRET = os.getenv("LASTFM_API_SECRET")
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
+EXCLUDED_WORDS = ["intro", "live", "cover", "remix", "re-mix"]
+
 artists = set()
 network = pylast.LastFMNetwork(LASTFM_API_KEY, LASTFM_API_SECRET)
 for player in arguments["<player>"]:
@@ -52,8 +54,13 @@ while True:
         items = sp.search(
             f"artist:{artist.item.get_name()} track:{track.item.get_name()}"
         )["tracks"]["items"]
-        track_found = len(items) > 0
-    track = items[0]
+        filtered_items = [
+            item
+            for item in items
+            if not any(word in item["name"].lower() for word in EXCLUDED_WORDS)
+        ]
+        track_found = len(filtered_items) > 0
+    track = filtered_items[0]
 
     while not any(device["is_active"] for device in sp.devices()["devices"]):
         input("Activate a Spotify device by playing a song and press Enter")
